778919e8f7b611eb6b873d5e4bf666d41762d6b0
camrockz <55742787+camrockz@users.noreply.github.com>
2020-07-19 01:43:12 +1000

    Version 2.0

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
 # Cloudflare DDNS Updater
 
-A simple executable to check the IP address that points to the current machine, and update cloudflare's DDNS system with our domain name.
+A simple executable to check the IP address that points to the current machine, and update Cloudflare's DDNS system with our domain name.
 
-Requirements
+## Requirements
 
 - C99 libc and compiler
 - Libcurl
+- Libjson-c
 - CMake 3.0+ (optional)
+
+## Building
+
+```
+git clone https://github.com/camrockz/Cloudflare-DDNS-Updater.git
+mkdir build
+cd /build
+```
+If you are using CMake
+
+```
+cmake .. -DCMAKE_BUILD_TYPE=release
+make
+```
+Else use GCC or Clang
+
+```
+gcc -O3 -DNDEBUG -o ipup ../stable.c -lcurl -ljson-c
+```
+## Running
+
+Rename example-config.json to config.json and replace the strings inside the double quotes **" "** with those that apply to your Cloudflare account. Your key can be found in your Cloudflare account settings.
+
+Run as a cron job every 60 seconds.
+
+```
+* * * * * cd /path/to/build/directory/ && ./ipup
+```
--- stable.c
+++ stable.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
+#include <json-c/json.h>
 
 struct config
 {
-    char email[64];
-    char key[64];
-    char record[64];
-    char zoneID[64];
-    char recordID[64];
+    const char *email;
+    const char *key;
+    const char *record;
+    const char *zoneID;
+    const char *recordID;
 };
 
 struct memory
@@ -30,17 +31,17 @@ int main()
     CURLcode res;
     FILE *fp;
     FILE *log;
-    char buff[64];
-    char buff2[128];
-    char buff3[128];
-    char url[256];
-    char data[256];
-    
-    static struct memory ip;
-    static struct memory result;
+    char buff[128], buff2[128], buff3[128];
+    char filebuff[4096];
+    char url[512], urlZoneID[512], urlRecordID[512];
+    char data[512];
+    json_object *parsed, *parsed2, *parsed3, *parsed4, *email, *key, *record, *zoneID,*recordID, *jobResult, *arrayFirst, *success;
+    struct curl_slist *headers = NULL;
+    struct memory ip, result, result2, result3;
+    json_bool firstRun = FALSE;
+
     struct config *conf = malloc(sizeof(struct config));
-    
-    log = fopen("ipup.log", "a");
+    log = fopen("../ipup.log", "a");
     
     curl_global_init(CURL_GLOBAL_DEFAULT);
     
@@ -55,10 +56,11 @@ int main()
         {
             fprintf(stderr, "%s: curl_easy_perform() failed to get IP: %s\n", timestamp(), curl_easy_strerror(res));
             fprintf(log, "%s: curl_easy_perform() failed to get IP: %s\n", timestamp(), curl_easy_strerror(res));
+            fclose(log);
             exit(EXIT_FAILURE);
         }
     }
-    if ((fp = fopen("ip", "r")) != NULL)
+    if ((fp = fopen(".ip", "r")) != NULL)
     {
         getString(buff, (sizeof(buff)), fp);
         fclose(fp);
@@ -69,62 +71,128 @@ int main()
     if (!strcmp(ip.response, buff))
     {
         printf("IP has not changed!\n");
+        curl_easy_cleanup(curl);
+        fclose(log);
         exit(EXIT_SUCCESS);
     }
     fprintf(log, "%s: IP has changed to %s\n", timestamp(), ip.response);
     fprintf(stdout, "%s: IP has changed to %s\n", timestamp(), ip.response);
-    curl_easy_cleanup(curl);
     
-    if ((fp = fopen("conf", "r")) == NULL)
+    if ((fp = fopen("../config.json", "r")) == NULL)
     {
-        fprintf(log, "%s: FILE ERROR - could not open conf file\n", timestamp());
-        fprintf(stdout, "%s: FILE ERROR - could not open conf file\n", timestamp());
+        fprintf(log, "%s: FILE ERROR - could not open config.json file\n", timestamp());
+        fprintf(stdout, "%s: FILE ERROR - could not open config.json file\n", timestamp());
+        fclose(log);
         exit(EXIT_FAILURE);
     }
-    getString(conf->email, sizeof(conf->email), fp);
-    getString(conf->key, sizeof(conf->key), fp);
-    getString(conf->record, sizeof(conf->record), fp);
-    getString(conf->zoneID, sizeof(conf->zoneID), fp);
-    getString(conf->recordID, sizeof(conf->recordID), fp);
-    fclose(fp);
+    fread(filebuff, sizeof(filebuff), 1, fp);
+    parsed = json_tokener_parse(filebuff);
+    json_object_object_get_ex(parsed, "email", &email);
+    conf->email = json_object_get_string(email);
+    json_object_object_get_ex(parsed, "key", &key);
+    conf->key = json_object_get_string(key);
+    json_object_object_get_ex(parsed, "record", &record);
+    conf->record = json_object_get_string(record);
+    json_object_object_get_ex(parsed, "zoneID", &zoneID);
+    json_object_object_get_ex(parsed, "recordID", &recordID);
+    if (json_object_get_string(zoneID) == NULL || json_object_get_string(recordID) == NULL)
+    {
+        if(curl)
+        {
+            firstRun = TRUE;
+            headers = curl_slist_append(headers, "Accept: application/json");
+            headers = curl_slist_append(headers, "Content-Type: application/json");
+        
+            snprintf(buff2, sizeof(buff2), "X-Auth-Email: %s", conf->email);
+            headers = curl_slist_append(headers, buff2);
+        
+            snprintf(buff3, sizeof(buff3), "X-Auth-Key: %s", conf->key);
+            headers = curl_slist_append(headers, buff3);
+            snprintf(urlZoneID, sizeof(urlZoneID), "https://api.cloudflare.com/client/v4/zones?name=%s", conf->record);
+        
+            curl_easy_setopt(curl, CURLOPT_URL, urlZoneID);
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&result);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+            {
+                fprintf(log, "%s: curl_easy_perform() failed to get zoneID: %s\n", timestamp(), curl_easy_strerror(res));
+                fprintf(stderr, "%s: curl_easy_perform() failed to get zoneID: %s\n", timestamp(), curl_easy_strerror(res));
+                fclose(log);
+                exit(EXIT_FAILURE);
+            }
+            parsed2 = json_tokener_parse(result.response);
+            free(result.response);
+            json_object_object_get_ex(parsed2, "result", &jobResult);
+            arrayFirst = json_object_array_get_idx(jobResult, 0);
+            json_object_object_get_ex(arrayFirst, "id", &zoneID);
+            conf->zoneID = json_object_get_string(zoneID);
+            snprintf(urlRecordID, sizeof(urlRecordID), "https://api.cloudflare.com/client/v4/zones/%s/dns_records?name=%s", conf->zoneID, conf->record);
+            curl_easy_setopt(curl, CURLOPT_URL, urlRecordID);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&result2);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+            {
+                fprintf(log, "%s: curl_easy_perform() failed to get recordID: %s\n", timestamp(), curl_easy_strerror(res));
+                fprintf(stderr, "%s: curl_easy_perform() failed to get recordID: %s\n", timestamp(), curl_easy_strerror(res));
+                fclose(log);
+                exit(EXIT_FAILURE);
+            }
+            parsed3 = json_tokener_parse(result2.response);
+            free(result2.response);
+            json_object_object_get_ex(parsed3, "result", &jobResult);
+            arrayFirst = json_object_array_get_idx(jobResult, 0);
+            json_object_object_get_ex(arrayFirst, "id", &recordID);
+            conf->recordID = json_object_get_string(recordID);
+            fclose(fp);
+            json_object_object_add(parsed, "zoneID", zoneID);
+            json_object_get(zoneID);
+            json_object_object_add(parsed, "recordID", recordID);
+            json_object_get(recordID);
+            json_object_to_file_ext("../config.json", parsed, JSON_C_TO_STRING_PRETTY);
+        }
+
+    }
+    else
+    {
+    conf->zoneID = json_object_get_string(zoneID);
+    conf->recordID = json_object_get_string(recordID);
+    headers = curl_slist_append(headers, "Accept: application/json");
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    snprintf(buff2, sizeof(buff2), "X-Auth-Email: %s", conf->email);
+    headers = curl_slist_append(headers, buff2);
+    snprintf(buff3, sizeof(buff3), "X-Auth-Key: %s", conf->key);
+    headers = curl_slist_append(headers, buff3);
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    }
+    
     snprintf(data, sizeof(data), "{\"id\":\"%s\",\"type\":\"A\",\"name\":\"%s\",\"content\":\"%s\",\"ttl\":120}", conf->zoneID, conf->record, ip.response);
     snprintf(url, sizeof(url), "https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s", conf->zoneID, conf->recordID);
-    
-    curl = curl_easy_init();
     if(curl)
     {
-        struct curl_slist *headers = NULL;
-        headers = curl_slist_append(headers, "Accept: application/json");
-        headers = curl_slist_append(headers, "Content-Type: application/json");
-        
-        snprintf(buff2, sizeof(buff2), "X-Auth-Email: %s", conf->email);
-        headers = curl_slist_append(headers, buff2);
-        
-        snprintf(buff3, sizeof(buff3), "X-Auth-Key: %s", conf->key);
-        headers = curl_slist_append(headers, buff3);
-        
+
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcrp/0.1");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&result);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&result3);
         res = curl_easy_perform(curl);
         if(res != CURLE_OK)
         {
             fprintf(log, "%s: curl_easy_perform() failed to update IP with cloudflare: %s\n", timestamp(), curl_easy_strerror(res));
             fprintf(stderr, "%s: curl_easy_perform() failed to update IP with cloudflare: %s\n", timestamp(), curl_easy_strerror(res));
+            fclose(log);
             exit(EXIT_FAILURE);
         }
         else
         {
-            char good[] = {"success\":true,\"errors\":[]"};
-            if (strstr(result.response, good) != NULL)
+            parsed4 = json_tokener_parse(result3.response);
+            json_object_object_get_ex(parsed4, "success", &success);
+            if (json_object_get_boolean(success) == TRUE)
             {
                 fprintf(log, "%s: SUCCESS - updated IP with cloudflare to %s\n", timestamp(), ip.response);
                 fprintf(stdout, "%s: SUCCESS - updated IP with cloudflare to %s\n", timestamp(), ip.response);
-                if ((fp = fopen("ip", "w")) != NULL)
+                if ((fp = fopen(".ip", "w")) != NULL)
                 {
                     fprintf(fp, "%s", ip.response);
                     fclose(fp);
@@ -137,15 +205,24 @@ int main()
             }
             else
             {
-                fprintf(log, "%s: FAILURE - dumping result: %s\n", timestamp(), result.response);
-                fprintf(stdout, "%s: FAILURE - dumping result: %s\n", timestamp(), result.response);
+                fprintf(log, "%s: FAILURE - dumping result: %s\n", timestamp(), result3.response);
+                fprintf(stdout, "%s: FAILURE - dumping result: %s\n", timestamp(), result3.response);
             }
         }
         curl_easy_cleanup(curl);
+        free(result3.response);
+        json_object_put(parsed);
+        if (firstRun == TRUE)
+        {
+            json_object_put(parsed2);
+            json_object_put(parsed3);
+        }
+        json_object_put(parsed4);
     }
     
 curl_global_cleanup();
 free(conf);
+free(ip.response);
 fclose(log);
 return 0;
 }
@@ -208,7 +285,7 @@ static size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp)
     char *ptr = realloc(mem->response, mem->size + realsize + 1);
     if(ptr == NULL)
     return 0;  /* out of memory! */
- 
+    
     mem->response = ptr;
     memcpy(&(mem->response[mem->size]), buffer, realsize);
     mem->size += realsize;
